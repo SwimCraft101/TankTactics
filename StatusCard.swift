160720b1bfbaa11baf2e68c4f1ae1e27dd45d810
Hilton Sherrard <hilton@sherrard.ws>
2024-08-09 09:49:35 -0400

    Addind assets for Status Cards

---
+++ StatusCard.swift
@@ -0,0 +1,18 @@
+//
+//  StatusCard.swift
+//  TankTactics
+//
+//  Created by Hilton Sherrard on 8/9/24.
+//
+
+import SwiftUI
+
+struct StatusCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatusCard()
+}
