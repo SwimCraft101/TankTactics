9addf3c7c627b235e027061b733abb136e0edbbf
Hilton Sherrard <hilton@sherrard.ws>
2024-08-22 13:59:55 -0400

    Cleaned up old system

--- TankTactics/TankTacticsApp.swift
+++ TankTactics/TankTacticsApp.swift
@@ -12,7 +12,7 @@ struct TankTacticsApp: App {
     var body: some Scene {
         WindowGroup {
             HSplitView {
-                Viewport(board: board, coordinates: Coordinates(x: 0, y: 0), cellSize: 35, viewRenderSize: 7, highDetailSightRange: 1000000, lowDetailSightRange: 1000000, radarRange: 1000000)
+                Viewport(coordinates: Coordinates(x: 0, y: 0), cellSize: 35, viewRenderSize: 7, highDetailSightRange: 1000000, lowDetailSightRange: 1000000, radarRange: 1000000)
                 VStack {
                     HStack {
                         Spacer()
